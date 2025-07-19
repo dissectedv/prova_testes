44347b8fd48d2fe359bb6e65dd4276070a207969
João Victor <joaovictorsampaio393@gmail.com>
2025-07-18 22:02:19 -0400

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,9 @@
-Cypress.Commands.add('login', (username, password) => {
-  cy.get('#user-name').clear().type(username)
-  cy.get('#password').clear().type(password)
-  cy.get('#login-button').click()
+Cypress.Commands.add('login_sauce', (userName, password) => {
+    cy.get('#user-name').type(userName)
+    cy.get('#password').type(password)
+    cy.get('#login-button').click()
+})
+Cypress.Commands.add('login_sauce_without_username', (password) => {
+    cy.get('#password').type(password)
+    cy.get('#login-button').click()
 })
