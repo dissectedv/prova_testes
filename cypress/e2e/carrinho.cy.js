f3919f2c79de7241a9ba81db519abbb7a030ef0f
João Victor <joaovictorsampaio393@gmail.com>
2025-07-18 23:49:23 -0400

    Update carrinho.cy.js

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,10 +1,14 @@
-describe('Automatizando testes no sauce demo', () => {
-    beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/')
-    })
-    it('Teste adicionar produto no carrinho', () => {   
-        cy.login_sauce('standard_user', 'secret_sauce')
-        cy.get('#add-to-cart-sauce-labs-backpack').click()
-        cy.get('button').contains('Remove').should('be.visible')
-    })
+describe('COMPRA_001 - Adicionar item ao carrinho', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+  })
+
+  it('Deve adicionar item ao carrinho com sucesso', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('.shopping_cart_link').click()
+    cy.get('.cart_item').should('exist')
+  })
 })
