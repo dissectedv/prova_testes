80770e21670df0adf8ff9aad0f1e5d666d106b3c
João Victor <joaovictorsampaio393@gmail.com>
2025-07-18 23:43:14 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-  baseUrl: 'https://www.saucedemo.com',
-  pageLoadTimeout: 120000,
-  chromeWebSecurity: false
-}
+    baseUrl: 'https://www.saucedemo.com',
+    pageLoadTimeout: 120000,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config
     },
   },
 })
