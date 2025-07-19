ee988e8edaaa3e75be7f38ccf332dbc779c4c91f
João Victor <joaovictorsampaio393@gmail.com>
2025-07-18 22:01:08 -0400

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,33 +1,22 @@
-describe('Testes de Sanidade na Página de Login - Sauce Demo', () => {
-
+describe('Automatizando testes no sauce demo', () => {
     beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/');
-    });
-
-    it('LOGIN_001: Deve realizar login com sucesso usando credenciais válidas (standard_user)', () => {
-        cy.login('standard_user', 'secret_sauce');
-
-        cy.url().should('include', '/inventory.html');
-        cy.get('.title').should('contain', 'Products');
-    });
-
-    it('LOGIN_002: Deve exibir mensagem de erro para usuário inválido', () => {
-        cy.login('usuario_invalido', 'secret_sauce');
-
-        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service');
-        cy.url().should('eq', 'https://www.saucedemo.com/');
-    });
-
-    it('LOGIN_003: Deve exibir mensagem de erro para senha inválida', () => {
-        cy.login('standard_user', 'senha_invalida');
-        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service');
-        cy.url().should('eq', 'https://www.saucedemo.com/');
-    });
-
-    it('LOGIN_004: Deve exibir mensagem de erro para usuário bloqueado (locked_out_user)', () => {
-        cy.login('locked_out_user', 'secret_sauce');
-        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Sorry, this user has been locked out.');
-        cy.url().should('eq', 'https://www.saucedemo.com/');
-    });
-
-});
+        cy.visit('https://www.saucedemo.com/')
+    })
+    it('Teste com usuário locked_out_user ', () => {     
+        cy.login_sauce('locked_out_user', 'secret_sauce')
+        cy.get('h3').contains('Epic sadface: Sorry, this user has been locked out.').should('be.visible')
+    })
+    it('Teste com senha incorreta ', () => {     
+        cy.login_sauce('standard_user', 'incorrectpassword')
+        cy.get('h3').contains('Epic sadface: Username and password do not match any user in this service').should('be.visible')
+    })
+    it('Teste sem informar username ', () => {     
+        cy.login_sauce_without_username('secret_sauce')
+        cy.get('h3').contains('Epic sadface: Username is required').should('be.visible')
+    })
+    it('Teste adicionar produto no carrinho', () => {   
+        cy.login_sauce('standard_user', 'secret_sauce')
+        cy.get('#add-to-cart-sauce-labs-backpack').click()
+        cy.get('button').contains('Remove').should('be.visible')
+    })
+})
